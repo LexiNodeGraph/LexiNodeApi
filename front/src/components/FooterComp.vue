67f1b3b83f293e65080ee47b3f363c0abf3404f0
Raphael-Soares <soaresphaell@gmail.com>
2022-06-07 12:50:38 -0300

    front

---
+++ front/src/components/FooterComp.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="footer">22 Copyright: KEYPHI</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.footer {
+  position:relative;
+  bottom:0;
+  width:100%;
+  text-align: center; 
+  padding-block: 10px;
+  background-color: black;
+  color: aliceblue;
+
+
+}
+
+</style>
---
+++ front/src/components/NavbarComp.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="navbar">
+
+        <div class="row lexinode">
+            <div class="lexi">LEXI</div><div class="node">NODE</div> 
+        </div>
+
+        <div class="row push">
+            <a href="#" class="item">ARTIGOS</a>
+            <a href="#" class="item push">AUTORES</a>
+        </div>
+
+        <div class="row ">
+            <a href="#" class="item ">CRIAR CONTA</a>
+            <a href="#" class="item ">ENTRAR</a>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+
+    }
+  }
+
+}
+</script>
+
+<style >
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+
+
+.navbar {
+    font-size: 2rem;
+    font-family: 'Bebas Neue', 'cursive';
+    color: white;
+
+    padding: 15px;
+    
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+
+}
+.row{
+    display: flex;
+    align-items: center;
+    
+}
+.item {
+    text-decoration: none;
+    color: white;
+    margin-inline: 20px;
+    transition: 0.2s;
+}
+
+.item:hover {
+    color: #0952DB;
+}
+
+.push {
+    margin-right: auto;
+}
+
+.lexinode {
+    display: flex;
+    font-size: 3rem;
+    margin-right: 20px;
+}
+
+.lexinode .node {
+    color:#0952DB;
+}
+
+
+
+
+</style>
+
+
